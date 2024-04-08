8e19fdda7ff09d020920b00d1e72988029147648
Valentin Palkovic <valentin@chromatic.com>
2024-04-08 15:27:21 +0200

    Create channel to create a new story

--- code/addons/controls/src/constants.ts
+++ code/addons/controls/src/constants.ts
@@ -2,3 +2,5 @@ export const ADDON_ID = 'addon-controls' as const;
 export const PARAM_KEY = 'controls' as const;
 export const FILE_COMPONENT_SEARCH = 'file-component-search' as const;
 export const FILE_COMPONENT_SEARCH_RESULT = 'file-component-search-result' as const;
+export const CREATE_NEW_STORYFILE = 'create-new-storyfile' as const;
+export const CREATE_NEW_STORYFILE_RESULT = 'create-new-storyfile-result' as const;
